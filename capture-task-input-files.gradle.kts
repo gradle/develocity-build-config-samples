c33cb23c80ca8c4450a7d9fee473c160a60098f7
Etienne Studer <etienne@gradle.com>
2019-09-23 18:53:31 +0200

    Remove scripts that are covered in the all-inclusive script

--- capture-task-input-files.gradle.kts
+++
@@ -1,6 +0,0 @@
-// With Gradle Enterprise, the build scan plugin v2.1+, and Gradle 5.0+, we can capture hashes of task inputs for
-// use with build comparisons. See https://docs.gradle.com/enterprise/tutorials/task-inputs-comparison/ for more
-// information.
-buildScan {
-    isCaptureTaskInputFiles = true
-}
