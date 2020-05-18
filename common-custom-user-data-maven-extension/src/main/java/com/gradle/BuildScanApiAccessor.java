63eb2892081b8c4f2afd95b3d04f03b44992e86e
Marc Philipp <marc@gradle.com>
2020-05-18 17:18:53 +0200

    Resolve deprecation warning
    Use PlexusContainer instead of MavenSession to lookup BuildScanApi.

--- common-custom-user-data-maven-extension/src/main/java/com/gradle/BuildScanApiAccessor.java
+++ common-custom-user-data-maven-extension/src/main/java/com/gradle/BuildScanApiAccessor.java
@@ -2,7 +2,7 @@ package com.gradle;
 
 import com.gradle.maven.extension.api.scan.BuildScanApi;
 import org.apache.maven.MavenExecutionException;
-import org.apache.maven.execution.MavenSession;
+import org.codehaus.plexus.PlexusContainer;
 import org.codehaus.plexus.classworlds.realm.ClassRealm;
 import org.codehaus.plexus.component.repository.exception.ComponentLookupException;
 
@@ -11,15 +11,14 @@ import static java.util.Comparator.comparing;
 final class BuildScanApiAccessor {
 
     private static final String PACKAGE = "com.gradle.maven.extension.api.scan";
-    private static final String BUILD_SCAN_API_SESSION_OBJECT = PACKAGE + ".BuildScanApi";
+    private static final String BUILD_SCAN_API_CONTAINER_OBJECT = PACKAGE + ".BuildScanApi";
 
-    @SuppressWarnings("deprecation")
-    static BuildScanApi lookup(MavenSession session, Class<?> extensionClass) throws MavenExecutionException {
+    static BuildScanApi lookup(PlexusContainer container, Class<?> extensionClass) throws MavenExecutionException {
         ensureBuildScanApiIsAccessible(extensionClass);
         try {
-            return (BuildScanApi) session.lookup(BUILD_SCAN_API_SESSION_OBJECT);
+            return (BuildScanApi) container.lookup(BUILD_SCAN_API_CONTAINER_OBJECT);
         } catch (ComponentLookupException e) {
-            throw new MavenExecutionException(String.format("Cannot look up object in session: %s", BUILD_SCAN_API_SESSION_OBJECT), e);
+            throw new MavenExecutionException(String.format("Cannot look up object in container: %s", BUILD_SCAN_API_CONTAINER_OBJECT), e);
         }
     }
 
@@ -32,15 +31,15 @@ final class BuildScanApiAccessor {
             ClassRealm extensionRealm = (ClassRealm) classLoader;
             if (!"maven.ext".equals(extensionRealm.getId())) {
                 extensionRealm.getWorld().getRealms().stream()
-                    .filter(realm -> realm.getId().contains("com.gradle:gradle-enterprise-maven-extension") || realm.getId().equals("maven.ext"))
-                    .max(comparing((ClassRealm realm) -> realm.getId().length()))
-                    .ifPresent(realm -> {
-                        try {
-                            extensionRealm.importFrom(realm.getId(), PACKAGE);
-                        } catch (Exception e) {
-                            throw new RuntimeException("Could not import package from realm with id " + realm.getId(), e);
-                        }
-                    });
+                        .filter(realm -> realm.getId().contains("com.gradle:gradle-enterprise-maven-extension") || realm.getId().equals("maven.ext"))
+                        .max(comparing((ClassRealm realm) -> realm.getId().length()))
+                        .ifPresent(realm -> {
+                            try {
+                                extensionRealm.importFrom(realm.getId(), PACKAGE);
+                            } catch (Exception e) {
+                                throw new RuntimeException("Could not import package from realm with id " + realm.getId(), e);
+                            }
+                        });
             }
         }
     }
--- common-custom-user-data-maven-extension/src/main/java/com/gradle/CommonCustomUserDataMavenExtension.java
+++ common-custom-user-data-maven-extension/src/main/java/com/gradle/CommonCustomUserDataMavenExtension.java
@@ -4,19 +4,29 @@ import com.gradle.maven.extension.api.scan.BuildScanApi;
 import org.apache.maven.AbstractMavenLifecycleParticipant;
 import org.apache.maven.MavenExecutionException;
 import org.apache.maven.execution.MavenSession;
+import org.codehaus.plexus.PlexusContainer;
 import org.codehaus.plexus.component.annotations.Component;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
 
 @Component(role = AbstractMavenLifecycleParticipant.class)
 public final class CommonCustomUserDataMavenExtension extends AbstractMavenLifecycleParticipant {
 
     private static final Logger LOG = LoggerFactory.getLogger(CommonCustomUserDataMavenExtension.class);
 
+    private final PlexusContainer container;
+
+    @Inject
+    public CommonCustomUserDataMavenExtension(PlexusContainer container) {
+        this.container = container;
+    }
+
     @Override
     public void afterProjectsRead(MavenSession session) throws MavenExecutionException {
         LOG.debug("Executing extension: {}", getClass().getSimpleName());
-        BuildScanApi buildScan = BuildScanApiAccessor.lookup(session, getClass());
+        BuildScanApi buildScan = BuildScanApiAccessor.lookup(container, getClass());
         if (buildScan != null) {
             LOG.debug("Capturing custom user data in build scan");
             CustomUserData.addToBuildScan(buildScan);
