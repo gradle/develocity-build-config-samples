f39fb246db0c4c24e1e53966b513abaaa3087b84
Etienne Studer <etienne@gradle.com>
2020-06-11 13:00:41 +0200

    Avoid dependency on slf4j

--- common-custom-user-data-maven-extension/src/main/java/com/gradle/CommonCustomUserDataMavenExtension.java
+++ common-custom-user-data-maven-extension/src/main/java/com/gradle/CommonCustomUserDataMavenExtension.java
@@ -6,31 +6,30 @@ import org.apache.maven.MavenExecutionException;
 import org.apache.maven.execution.MavenSession;
 import org.codehaus.plexus.PlexusContainer;
 import org.codehaus.plexus.component.annotations.Component;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.codehaus.plexus.logging.Logger;
 
 import javax.inject.Inject;
 
 @Component(role = AbstractMavenLifecycleParticipant.class)
 public final class CommonCustomUserDataMavenExtension extends AbstractMavenLifecycleParticipant {
 
-    private static final Logger LOG = LoggerFactory.getLogger(CommonCustomUserDataMavenExtension.class);
-
     private final PlexusContainer container;
+    private final Logger logger;
 
     @Inject
-    public CommonCustomUserDataMavenExtension(PlexusContainer container) {
+    public CommonCustomUserDataMavenExtension(PlexusContainer container, Logger logger) {
         this.container = container;
+        this.logger = logger;
     }
 
     @Override
     public void afterProjectsRead(MavenSession session) throws MavenExecutionException {
-        LOG.debug("Executing extension: {}", getClass().getSimpleName());
+        logger.debug("Executing extension: " + getClass().getSimpleName());
         BuildScanApi buildScan = BuildScanApiAccessor.lookup(container, getClass());
         if (buildScan != null) {
-            LOG.debug("Capturing custom user data in build scan");
+            logger.debug("Capturing custom user data in build scan");
             CustomUserData.addToBuildScan(buildScan);
-            LOG.debug("Finished capturing custom user data in build scans");
+            logger.debug("Finished capturing custom user data in build scans");
         }
     }
 
