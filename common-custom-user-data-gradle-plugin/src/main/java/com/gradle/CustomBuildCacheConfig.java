fbbec6c9769f66f0b7273f0798f7f10d343bf005
Daz DeBoer <daz@gradle.com>
2021-01-13 18:25:45 -0700

    Add an example of build cache configuration

--- common-custom-user-data-gradle-plugin/src/main/java/com/gradle/CustomBuildCacheConfig.java
+++ common-custom-user-data-gradle-plugin/src/main/java/com/gradle/CustomBuildCacheConfig.java
@@ -1,15 +1,31 @@
 package com.gradle;
 
-
 import org.gradle.caching.configuration.BuildCacheConfiguration;
 
+/**
+ * Provide standardized build cache configuration.
+ * By applying the plugin, these build cache settings will automatically be applied.
+ */
 final class CustomBuildCacheConfig {
 
     static void configureBuildCache(BuildCacheConfiguration buildCache) {
-        // custom config goes here
+        /* Example of build cache configuration
+
+        boolean isCiServer = System.getenv().containsKey("CI");
+
+        buildCache.local(local -> {
+            local.setEnabled(!isCiServer);
+        });
+
+        buildCache.remote(HttpBuildCache.class, remote -> {
+            remote.setUrl("https://localhost/cache/");
+            remote.setAllowUntrustedServer(true);
+            remote.setPush(isCiServer);
+        });
+
+        */
     }
 
     private CustomBuildCacheConfig() {
     }
-
 }
