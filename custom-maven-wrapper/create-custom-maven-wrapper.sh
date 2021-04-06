5096c13924663a1b78897206d12b36667cb8098b
Jim Hurne <jhurne@gradle.com>
2021-04-06 10:21:43 -0400

    Exit script explitily if cd fails
    Suggested by shellceck. See
    https://github.com/koalaman/shellcheck/wiki/SC2164

--- custom-maven-wrapper/create-custom-maven-wrapper.sh
+++ custom-maven-wrapper/create-custom-maven-wrapper.sh
@@ -20,7 +20,7 @@ nc='\033[0m'
 
 if [ -n "$1" ]
 then
-  cd "$1"
+  cd "$1" || exit
 fi
 
 echo -e "${yellow}Installing Maven wrapper${nc}"
@@ -29,6 +29,6 @@ mvn -N io.takari:maven:0.7.7:wrapper -Dmaven=$maven_version
 echo -e "${yellow}Customizing Maven wrapper script by adding a custom 'Wrapper' tag${nc}"
 grep -q 'scan.tag.Wrapper' mvnw || sed -i '' $'s/^MAVEN_OPTS/&="-Dscan.tag.Wrapper $&"\\\n&/' mvnw
 
-cd "$current_dir"
+cd "$current_dir" || exit
 
 exit 0
