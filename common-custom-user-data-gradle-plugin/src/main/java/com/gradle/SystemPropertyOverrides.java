d3928c550cdcea73080d8f4e324b2bc32aac5bc3
Etienne Studer <etienne@gradle.com>
2021-05-28 10:11:59 +0200

    Polish

--- common-custom-user-data-gradle-plugin/src/main/java/com/gradle/SystemPropertyOverrides.java
+++ common-custom-user-data-gradle-plugin/src/main/java/com/gradle/SystemPropertyOverrides.java
@@ -15,16 +15,20 @@ import static com.gradle.Utils.withSysProperty;
  */
 final class SystemPropertyOverrides {
 
-    // system properties to override Gradle Enterprise, Build Cache, and Build Scan configuration
+    // system properties to override Gradle Enterprise configuration
     public static final String GRADLE_ENTERPRISE_URL = "gradle.enterprise.url";
+
+    // system properties to override local build cache configuration
     public static final String LOCAL_CACHE_DIRECTORY = "gradle.cache.local.directory";
     public static final String LOCAL_CACHE_REMOVE_UNUSED_ENTRIES_AFTER_DAYS = "gradle.cache.local.removeUnusedEntriesAfterDays";
     public static final String LOCAL_CACHE_ENABLED = "gradle.cache.local.enabled";
-    public static final String LOCAL_CACHE_PUSH_ENABLED = "gradle.cache.local.push";
+    public static final String LOCAL_CACHE_PUSH = "gradle.cache.local.push";
+
+    // system properties to override remote build cache configuration
     public static final String REMOTE_CACHE_URL = "gradle.cache.remote.url";
     public static final String REMOTE_CACHE_ALLOW_UNTRUSTED_SERVER = "gradle.cache.remote.allowUntrustedServer";
     public static final String REMOTE_CACHE_ENABLED = "gradle.cache.remote.enabled";
-    public static final String REMOTE_CACHE_PUSH_ENABLED = "gradle.cache.remote.push";
+    public static final String REMOTE_CACHE_PUSH = "gradle.cache.remote.push";
 
     static void configureGradleEnterprise(GradleEnterpriseExtension gradleEnterprise, ProviderFactory providers) {
         withSysProperty(GRADLE_ENTERPRISE_URL, gradleEnterprise::setServer, providers);
@@ -35,7 +39,7 @@ final class SystemPropertyOverrides {
             withSysProperty(LOCAL_CACHE_DIRECTORY, local::setDirectory, providers);
             withDurationSysProperty(LOCAL_CACHE_REMOVE_UNUSED_ENTRIES_AFTER_DAYS, v -> local.setRemoveUnusedEntriesAfterDays((int) v.toDays()), providers);
             withBooleanSysProperty(LOCAL_CACHE_ENABLED, local::setEnabled, providers);
-            withBooleanSysProperty(LOCAL_CACHE_PUSH_ENABLED, local::setPush, providers);
+            withBooleanSysProperty(LOCAL_CACHE_PUSH, local::setPush, providers);
         });
 
         // null check required to avoid creating a remote build cache instance when none was already present in the build
@@ -44,11 +48,12 @@ final class SystemPropertyOverrides {
                 withSysProperty(REMOTE_CACHE_URL, remote::setUrl, providers);
                 withBooleanSysProperty(REMOTE_CACHE_ALLOW_UNTRUSTED_SERVER, remote::setAllowUntrustedServer, providers);
                 withBooleanSysProperty(REMOTE_CACHE_ENABLED, remote::setEnabled, providers);
-                withBooleanSysProperty(REMOTE_CACHE_PUSH_ENABLED, remote::setPush, providers);
+                withBooleanSysProperty(REMOTE_CACHE_PUSH, remote::setPush, providers);
             });
         }
     }
 
     private SystemPropertyOverrides() {
     }
+
 }
