92603467fa101f0b7bd287e334619447a3e79a64
Nelson Osacky <nosacky@gradle.com>
2022-02-11 16:25:18 +0100

    Simplify code, clean up spacing.

--- build-data-capturing-maven-samples/capture-ge-extension-version/maven-ge-extension-version.groovy
+++ build-data-capturing-maven-samples/capture-ge-extension-version/maven-ge-extension-version.groovy
@@ -3,6 +3,7 @@ import com.gradle.maven.extension.api.scan.BuildScanApi
 /**
  * This Groovy script captures the Gradle Enterprise Maven Extension version as a custom value.
  */
-InputStream stream = BuildScanApi.class.getClassLoader().getResourceAsStream("com.gradle.scan.plugin.internal.meta.buildAgentVersion.txt")
-String buildAgentVersion = new Scanner(stream).useDelimiter("\\A").next()
+
+InputStream stream = BuildScanApi.class.classLoader.getResourceAsStream("com.gradle.scan.plugin.internal.meta.buildAgentVersion.txt")
+String buildAgentVersion = new Scanner(stream).next()
 buildScan.value("GE Maven extension version", buildAgentVersion)
--- build-data-capturing-maven-samples/capture-profiles/maven-profiles.groovy
+++ build-data-capturing-maven-samples/capture-profiles/maven-profiles.groovy
@@ -1,7 +1,9 @@
 import com.gradle.maven.extension.api.scan.BuildScanApi
+
 /**
  * This Groovy script captures the active profiles and add them as tags to the Build Scan.
  */
+
 buildScan.executeOnce('tag-profiles') { BuildScanApi buildScanApi ->
     project.activeProfiles.each { profile -> buildScanApi.tag profile.id }
 }
