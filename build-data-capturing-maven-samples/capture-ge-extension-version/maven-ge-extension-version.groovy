fef61220f2de89881c5ca158803c0d15f46640d4
Nelson Osacky <nosacky@gradle.com>
2022-02-12 12:52:33 +0100

    Use already available buildscan instead of import.

--- build-data-capturing-maven-samples/capture-ge-extension-version/maven-ge-extension-version.groovy
+++ build-data-capturing-maven-samples/capture-ge-extension-version/maven-ge-extension-version.groovy
@@ -1,9 +1,7 @@
-import com.gradle.maven.extension.api.scan.BuildScanApi
-
 /**
  * This Groovy script captures the Gradle Enterprise Maven Extension version as a custom value.
  */
 
-InputStream stream = BuildScanApi.class.classLoader.getResourceAsStream("com.gradle.scan.plugin.internal.meta.buildAgentVersion.txt")
+InputStream stream = buildScan.class.classLoader.getResourceAsStream("com.gradle.scan.plugin.internal.meta.buildAgentVersion.txt")
 String buildAgentVersion = new Scanner(stream).next()
 buildScan.value("GE Maven extension version", buildAgentVersion)
