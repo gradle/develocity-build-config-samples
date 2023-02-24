6e00167b408d40ec3b7712120568e55397d9c4c8
Inaki Villar <inaki.seri@gmail.com>
2023-02-24 10:25:37 -0800

    adding sample for Spotless

---
+++ build-caching-maven-samples/spotless-project/src/main/java/com/example/App.java
@@ -0,0 +1,14 @@
+package com.example;
+
+/** Messy code makes programmers life harder. */
+public class App {
+
+    public static final String myConst =
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut"
+            + " labore ";
+
+    public static void main(String[] args) {
+        System.out.println(myConst);
+        System.out.println("Please fix 4me :)");
+    }
+}
