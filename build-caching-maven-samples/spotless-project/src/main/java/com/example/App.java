088c623eb8faae8a568b1b866b3f76bd167a2685
Inaki Villar <inaki.seri@gmail.com>
2023-03-02 10:26:04 -0800

    filtering by goal

--- build-caching-maven-samples/spotless-project/src/main/java/com/example/App.java
+++ build-caching-maven-samples/spotless-project/src/main/java/com/example/App.java
@@ -1,14 +1,7 @@
 package com.example;
 
-/** Messy code makes programmers life harder. */
 public class App {
-
-    public static final String myConst =
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut"
-            + " labore ";
-
-    public static void main(String[] args) {
-        System.out.println(myConst);
-        System.out.println("Please fix 4me :)");
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello" + "" + "" + "" + " World!");
+  }
 }
