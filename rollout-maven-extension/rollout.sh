b2d20b495b08ff7c43c832dcaeeb44f2f96d23d6
Nelson Osacky <nosacky@gradle.com>
2023-04-04 10:10:03 +0200

    Simplify checking out a specific branch

--- rollout-maven-extension/rollout.sh
+++ rollout-maven-extension/rollout.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 basedir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 repositories=$basedir/repositories.txt
@@ -10,14 +11,15 @@ nc='\033[0m'
 # -b option lets you specify a branch to checkout, store it in a variable
 while getopts b: option
 do
-case "${option}"
-in
-b) branch=${OPTARG};;
-esac
+  case "${option}" in
+    b) branch=${OPTARG};;
+    *) echo "Usage: $0 [-b branch_name]" >&2
+       exit 1;;
+  esac
 done
 
 # print branch name argument if specified, otherwise print a message saying the default branch will be counted
-if ( [ -z "$branch" ] ); then
+if [ -z "$branch" ]; then
   echo "No branch name specified, counting commits on default branch"
 else
   echo "Branch name is $branch"
@@ -44,15 +46,14 @@ function process_repositories() {
 function process_repository() {
   repository_name="${1##*/}"
 
-  # clone the Git epository without actually downloading files or history
-  git clone -n "$1" "$checkout_area/$repository_name" >& /dev/null
+  # Clone the repository branch if specified (otherwise default branch)
+  if [ -z "$branch" ]; then
+    git clone -n "$1" "$checkout_area/$repository_name" >& /dev/null
+  else
+    git clone -n -b "$branch" "$1" "$checkout_area/$repository_name"
+  fi
   pushd "$checkout_area/$repository_name" >& /dev/null || return
   git reset HEAD . >& /dev/null
-
-  # Check out the branch if option is specified
-  if ( [ ! -z "$branch" ] ); then
-    git checkout "$branch"
-  fi
 
   # append the number of git users by email in the last 30 days to a file
   git log --format="%ae" --since=30.day | sort -u >> "$basedir/gradle-enterprise-users.txt"
@@ -65,10 +66,7 @@ function cleanup() {
 }
 
 # entry point
-if [ ! -d ".mvn" ]; then
-  echo ".mvn directory is missing" >&2
-  exit 1
-elif [ ! -f "repositories.txt" ]; then
+if [ ! -f "repositories.txt" ]; then
   echo "repositories.txt file is missing" >&2
   exit 1
 else
