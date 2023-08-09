1481747dbb150ed649c63ca3b74371adc357d25e
Etienne Studer <etienne@gradle.com>
2023-08-09 19:06:38 +0200

    Polish Convention GE Gradle plugin (#820)
    * Change from GString to String
    
    * Use Gradle Toolchain feature
    
    * Update Javadoc
    
    * Update error messages
    
    * Display error messages for Gradle<5.0
    
    * Use GE Build Cache connector
    
    * Make exception messages consistent with CCUD plugin
    
    * Apply CCUD plugin from Convention GE plugin
    
    * Rename plugin class
    
    * Rename root project
    
    * Rename root project
    
    * Rename Convention plugin id
    
    * Rename Convention plugin id
    
    * Rename Convention plugin class
    
    * Apply CCUD Gradle plugin
    
    * Change plugin display name
    
    * Change the description
    
    * Fix typo in display name
    
    * Add example build for Gradle 4.0.2
    
    * Rename example-builds folder

--- convention-gradle-enterprise-gradle-plugin/plugins/gradle-2-through-4/src/main/java/com/myorg/ConventionGradleEnterpriseGradlePlugin.java
+++ convention-gradle-enterprise-gradle-plugin/plugins/gradle-2-through-4/src/main/java/com/myorg/ConventionGradleEnterpriseGradlePlugin.java
@@ -1,18 +1,20 @@
 package com.myorg;
 
+import com.gradle.CommonCustomUserDataGradlePlugin;
 import com.gradle.scan.plugin.BuildScanExtension;
 import com.gradle.scan.plugin.BuildScanPlugin;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.util.GradleVersion;
 
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * An example Gradle plugin for enabling and configuring Gradle Enterprise features (build scans only) for Gradle v2.0
- * through Gradle v4.10.2.
+ * An example Gradle plugin for enabling and configuring Gradle Enterprise features (Build Scan only) for
+ * Gradle versions 2.0 through Gradle v4.10.3.
  */
-public class BuildScansConventionsPlugin implements Plugin<Project> {
+public class ConventionGradleEnterpriseGradlePlugin implements Plugin<Project> {
 
     @Override
     public void apply(Project project) {
@@ -20,9 +22,20 @@ public class BuildScansConventionsPlugin implements Plugin<Project> {
         args.put("plugin", BuildScanPlugin.class);
         project.apply(args);
 
+        if (isGradle4OrNewer()) {
+            args = new HashMap<>();
+            args.put("plugin", CommonCustomUserDataGradlePlugin.class);
+            project.apply(args);
+        }
+
         /* Example of how to configure build scan publishing from the plugin. */
         BuildScanExtension buildScan = project.getExtensions().getByType(BuildScanExtension.class);
         buildScan.setServer("https://enterprise-samples.gradle.com");
         buildScan.publishAlways();
     }
+
+    private static boolean isGradle4OrNewer() {
+        return GradleVersion.current().compareTo(GradleVersion.version("4.0")) >= 0;
+    }
+
 }
---
+++ convention-gradle-enterprise-gradle-plugin/plugins/gradle-5-or-newer/src/main/java/com/myorg/ConventionGradleEnterpriseGradlePlugin.java
@@ -0,0 +1,89 @@
+package com.myorg;
+
+import com.gradle.CommonCustomUserDataGradlePlugin;
+import com.gradle.enterprise.gradleplugin.GradleEnterpriseExtension;
+import com.gradle.enterprise.gradleplugin.GradleEnterprisePlugin;
+import com.gradle.scan.plugin.BuildScanExtension;
+import com.gradle.scan.plugin.BuildScanPlugin;
+import org.gradle.api.GradleException;
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+import org.gradle.api.initialization.Settings;
+import org.gradle.caching.configuration.BuildCacheConfiguration;
+import org.gradle.util.GradleVersion;
+
+/**
+ * An example Gradle plugin for enabling and configuring Gradle Enterprise features for
+ * Gradle versions 5.x and higher.
+ */
+public class ConventionGradleEnterpriseGradlePlugin implements Plugin<Object> {
+
+    @Override
+    public void apply(Object target) {
+        if (target instanceof Settings) {
+            if (!isGradle6OrNewer()) {
+                throw new GradleException("For Gradle versions prior to 6.0, the Convention Gradle Enterprise plugin must be applied to the Root project");
+            }
+            configureGradle6OrNewer((Settings) target);
+        } else if (target instanceof Project) {
+            Project project = (Project) target;
+            if (!project.equals(project.getRootProject())) {
+                throw new GradleException("The Convention Gradle Enterprise plugin may only be applied to the Root project");
+            }
+
+            if (isGradle6OrNewer()) {
+                throw new GradleException("For Gradle versions 6.0 and newer, the Convention Gradle Enterprise plugin must be applied to Settings");
+            } else if (isGradle5OrNewer()) {
+                configureGradle5(project);
+            } else {
+                throw new GradleException("For Gradle versions prior to 5.0, the Convention Gradle Enterprise plugin is not supported");
+            }
+        }
+    }
+
+    private void configureGradle6OrNewer(Settings settings) {
+        settings.getPluginManager().apply(GradleEnterprisePlugin.class);
+        settings.getPluginManager().apply(CommonCustomUserDataGradlePlugin.class);
+        configureGradleEnterprise(settings.getExtensions().getByType(GradleEnterpriseExtension.class));
+        configureBuildCache(settings.getBuildCache(), settings.getExtensions().getByType(GradleEnterpriseExtension.class));
+    }
+
+    private void configureGradle5(Project project) {
+        project.getPluginManager().apply(BuildScanPlugin.class);
+        project.getPluginManager().apply(CommonCustomUserDataGradlePlugin.class);
+        configureGradleEnterprise(project.getExtensions().getByType(GradleEnterpriseExtension.class));
+        // configureBuildCache is not called because the build cache cannot be configured via a plugin prior to Gradle 6.0
+    }
+
+    private void configureGradleEnterprise(GradleEnterpriseExtension gradleEnterprise) {
+        // CHANGE ME: Apply your Gradle Enterprise Configuration here
+        gradleEnterprise.setServer("https://enterprise-samples.gradle.com");
+        configureBuildScan(gradleEnterprise.getBuildScan());
+    }
+
+    private void configureBuildScan(BuildScanExtension buildScan) {
+        buildScan.publishAlways();
+    }
+
+    private void configureBuildCache(BuildCacheConfiguration buildCache, GradleEnterpriseExtension gradleEnterprise) {
+        // CHANGE ME: Apply your Build Cache configuration here
+        boolean isCiServer = System.getenv().containsKey("CI");
+        buildCache.remote(gradleEnterprise.getBuildCache(), remote -> {
+            remote.setEnabled(true);
+            remote.setPush(isCiServer);
+        });
+        buildCache.local(local -> {
+            local.setEnabled(true);
+            local.setPush(true);
+        });
+    }
+
+    private static boolean isGradle6OrNewer() {
+        return GradleVersion.current().compareTo(GradleVersion.version("6.0")) >= 0;
+    }
+
+    private static boolean isGradle5OrNewer() {
+        return GradleVersion.current().compareTo(GradleVersion.version("5.0")) >= 0;
+    }
+
+}
