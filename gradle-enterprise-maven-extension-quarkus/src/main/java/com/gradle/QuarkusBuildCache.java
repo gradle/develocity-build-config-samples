f97a42409a0734d3702211e16f267a8c0d8b045f
Jerome Prinet <jprinet@gradle.com>
2023-08-30 14:15:09 +0200

    Remove commented output

--- gradle-enterprise-maven-extension-quarkus/src/main/java/com/gradle/QuarkusBuildCache.java
+++ gradle-enterprise-maven-extension-quarkus/src/main/java/com/gradle/QuarkusBuildCache.java
@@ -318,10 +318,6 @@ final class QuarkusBuildCache {
             outputs.file("quarkusExe", quarkusExeFileName);
             outputs.file("quarkusJar", quarkusJarFileName);
             outputs.file("quarkusUberJar", quarkusUberJarFileName);
-
-            // Do not declare dump config as goal output to avoid
-            // Goal execution marked as not cacheable: Build caching was not enabled for this goal execution because pre-existing files were modified. Cacheable goals may only create new files.
-            // outputs.file("quarkusProperties", QUARKUS_FILE_PROPERTIES_DUMP);
         });
     }
 }
