e6bba87830a574288e4067f10beaf3d782ba4f23
Jérôme Prinet <jprinet@gradle.com>
2024-07-15 15:43:38 +0200

    Add Quarkus extra dependencies for tests using Quarkus lower than 3.9.0 (#1302)

--- quarkus-build-caching-extension/src/main/java/com/gradle/QuarkusBuildCache.java
+++ quarkus-build-caching-extension/src/main/java/com/gradle/QuarkusBuildCache.java
@@ -4,7 +4,6 @@ import com.gradle.develocity.agent.maven.api.cache.BuildCacheApi;
 import com.gradle.develocity.agent.maven.api.cache.MojoMetadataProvider;
 import com.gradle.develocity.agent.maven.api.cache.NormalizationProvider;
 import org.apache.maven.artifact.DependencyResolutionRequiredException;
-import org.codehaus.plexus.util.xml.Xpp3Dom;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -39,7 +38,6 @@ final class QuarkusBuildCache {
     private static final String QUARKUS_CONFIG_KEY_GRAALVM_HOME = "quarkus.native.graalvm-home";
     private static final String QUARKUS_CONFIG_KEY_JAVA_HOME = "quarkus.native.java-home";
     private static final String PACKAGE_NATIVE = "native";
-    private static final String TEST_GOAL_KEY_ADD_QUARKUS_INPUTS = "addQuarkusInputs";
     // Quarkus' cacheable package types
     private static final List<String> QUARKUS_CACHEABLE_JAR_TYPES = Arrays.asList("jar", "legacy-jar", "uber-jar");
 
@@ -64,11 +62,13 @@ final class QuarkusBuildCache {
                 configureQuarkusBuildGoal(context, extensionConfiguration);
             });
             context.withPlugin("maven-surefire-plugin", () -> {
-                configureQuarkusExtraTestInputs(context, extensionConfiguration);
+                TestConfiguration testConfiguration = new TestConfiguration(context, extensionConfiguration);
+                configureQuarkusExtraTestInputs(context, extensionConfiguration, testConfiguration);
             });
             context.withPlugin("maven-failsafe-plugin", () -> {
-                configureQuarkusExtraTestInputs(context, extensionConfiguration);
-                configureQuarkusExtraIntegrationTestInputs(context, extensionConfiguration);
+                TestConfiguration testConfiguration = new TestConfiguration(context, extensionConfiguration);
+                configureQuarkusExtraTestInputs(context, extensionConfiguration, testConfiguration);
+                configureQuarkusExtraIntegrationTestInputs(context, testConfiguration);
             });
         });
     }
@@ -81,31 +81,26 @@ final class QuarkusBuildCache {
         }
     }
 
-    private void configureQuarkusExtraTestInputs(MojoMetadataProvider.Context context, QuarkusExtensionConfiguration extensionConfiguration) {
-        if (isQuarkusExtraTestInputsExpected(context, extensionConfiguration)) {
+    private void configureQuarkusExtraTestInputs(MojoMetadataProvider.Context context, QuarkusExtensionConfiguration extensionConfiguration, TestConfiguration testConfiguration) {
+        LOGGER.debug(QuarkusExtensionUtil.getLogMessage(testConfiguration.toString()));
+        if (testConfiguration.isAddQuarkusInputs()) {
             LOGGER.debug(QuarkusExtensionUtil.getLogMessage("Adding Quarkus extra test inputs"));
             context.inputs(inputs -> addQuarkusDependencyChecksumsInput(inputs, extensionConfiguration));
             context.inputs(inputs -> addQuarkusDependenciesInputs(inputs, extensionConfiguration));
         }
+        if (testConfiguration.isAddQuarkusPackageInputs()) {
+            LOGGER.debug(QuarkusExtensionUtil.getLogMessage("Adding Quarkus extra test package inputs"));
+            context.inputs(inputs -> addQuarkusJarInput(inputs, testConfiguration));
+        }
     }
 
-    private void configureQuarkusExtraIntegrationTestInputs(MojoMetadataProvider.Context context, QuarkusExtensionConfiguration extensionConfiguration) {
-        if (isQuarkusExtraTestInputsExpected(context, extensionConfiguration)) {
+    private void configureQuarkusExtraIntegrationTestInputs(MojoMetadataProvider.Context context, TestConfiguration testConfiguration) {
+        if (testConfiguration.isAddQuarkusInputs()) {
             context.inputs(this::addQuarkusArtifactPropertiesInput);
         }
-    }
-
-    private boolean isQuarkusExtraTestInputsExpected(MojoMetadataProvider.Context context, QuarkusExtensionConfiguration extensionConfiguration) {
-        if (extensionConfiguration.isQuarkusCacheEnabled()) {
-            Xpp3Dom properties = context.getMojoExecution().getConfiguration().getChild("properties");
-            if (properties != null) {
-                Xpp3Dom addQuarkusInputs = properties.getChild(TEST_GOAL_KEY_ADD_QUARKUS_INPUTS);
-                if (addQuarkusInputs != null) {
-                    return Boolean.parseBoolean(addQuarkusInputs.getValue());
-                }
-            }
+        if (testConfiguration.isAddQuarkusPackageInputs()) {
+            context.inputs(inputs -> addQuarkusExeInput(inputs, testConfiguration));
         }
-        return false;
     }
 
     private void configureQuarkusBuildGoal(MojoMetadataProvider.Context context, QuarkusExtensionConfiguration extensionConfiguration) {
@@ -286,6 +281,14 @@ final class QuarkusBuildCache {
 
     private void addQuarkusArtifactPropertiesInput(MojoMetadataProvider.Context.Inputs inputs) {
         inputs.fileSet("quarkusArtifactProperties", new File(TARGET_DIR + QUARKUS_ARTIFACT_PROPERTIES_FILE_NAME), fileSet -> fileSet.normalizationStrategy(MojoMetadataProvider.Context.FileSet.NormalizationStrategy.RELATIVE_PATH));
+    }
+
+    private void addQuarkusJarInput(MojoMetadataProvider.Context.Inputs inputs, TestConfiguration testConfiguration) {
+        inputs.fileSet("quarkusJarFile", new File(TARGET_DIR), fileSet -> fileSet.include(testConfiguration.getQuarkusJarFilePattern()).normalizationStrategy(MojoMetadataProvider.Context.FileSet.NormalizationStrategy.CLASSPATH));
+    }
+
+    private void addQuarkusExeInput(MojoMetadataProvider.Context.Inputs inputs, TestConfiguration testConfiguration) {
+        inputs.fileSet("quarkusExeFile", new File(TARGET_DIR), fileSet -> fileSet.include(testConfiguration.getQuarkusExeFilePattern()).normalizationStrategy(MojoMetadataProvider.Context.FileSet.NormalizationStrategy.RELATIVE_PATH));
     }
 
     private void addQuarkusDependenciesInputs(MojoMetadataProvider.Context.Inputs inputs, QuarkusExtensionConfiguration extensionConfiguration) {
