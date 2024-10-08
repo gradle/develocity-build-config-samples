1c3f3d9c2f44df6c31b9b775cc6f8768826a403c
Eric Haag <ehaag@gradle.com>
2024-10-08 11:40:50 -0500

    Remove Develocity agent version data capturing samples

--- build-data-capturing-maven-samples/capture-develocity-extension-version/maven-develocity-extension-version.groovy
+++
@@ -1,7 +0,0 @@
-/**
- * This Groovy script captures the Develocity Maven extension version as a custom value.
- */
-
-URL url = buildScan.buildScan.class.classLoader.getResource("com.gradle.scan.plugin.internal.meta.buildAgentVersion.txt")
-String buildAgentVersion = url.text
-buildScan.value("Develocity Maven extension version", buildAgentVersion)
