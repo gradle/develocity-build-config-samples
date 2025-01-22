5d558733709636aad60753935ca2a5e11c0e3889
Gasper Kojek <gkojek@gradle.com>
2025-01-22 13:08:50 +0100

    Handling of cp failures if source doesn't exist with bash strict mode

--- kotlin-scripts-caching-experiment/remote-cache-experiment-kotlin-scripts.sh
+++ kotlin-scripts-caching-experiment/remote-cache-experiment-kotlin-scripts.sh
@@ -27,10 +27,10 @@ echo "Initializing Gradle User Home directory at $homeDir"
 rm -rf $homeDir
 mkdir -p $homeDir
 mkdir -p $homeDir/caches/"$gradleVersion"/
-cp ~/.gradle/gradle.properties $homeDir
-cp -r ~/.gradle/caches/"$gradleVersion"/generated-gradle-jars $homeDir/caches/"$gradleVersion"/
-cp -r ~/.gradle/develocity/ $homeDir/develocity/
-cp -r ~/.gradle/enterprise/ $homeDir/enterprise/
+cp ~/.gradle/gradle.properties $homeDir || :
+cp -r ~/.gradle/caches/"$gradleVersion"/generated-gradle-jars $homeDir/caches/"$gradleVersion"/ || :
+cp -r ~/.gradle/develocity/ $homeDir/develocity/ || :
+cp -r ~/.gradle/enterprise/ $homeDir/enterprise/ || :
 
 # Note: This is expecting that CCUD Gradle plugin is applied
 export GRADLE_CACHE_REMOTE_PUSH=true
