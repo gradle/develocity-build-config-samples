83d60be801d5f3646dd87eb1600b53a0cce39c3b
Bot Githubaction <98587996+bot-githubaction@users.noreply.github.com>
2025-06-09 09:03:46 +0200

    Combined PRs (#1919)
    * Bump references to Common Custom User Data Maven extension from 2.0.2 to 2.0.3
    
    * Bump com.gradle:common-custom-user-data-maven-extension
    
    Bumps [com.gradle:common-custom-user-data-maven-extension](https://github.com/gradle/common-custom-user-data-maven-extension) from 2.0.2 to 2.0.3.
    - [Release notes](https://github.com/gradle/common-custom-user-data-maven-extension/releases)
    - [Commits](https://github.com/gradle/common-custom-user-data-maven-extension/compare/v2.0.2...v2.0.3)
    
    ---
    updated-dependencies:
    - dependency-name: com.gradle:common-custom-user-data-maven-extension
      dependency-version: 2.0.3
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump com.gradle:common-custom-user-data-maven-extension
    
    Bumps [com.gradle:common-custom-user-data-maven-extension](https://github.com/gradle/common-custom-user-data-maven-extension) from 2.0.2 to 2.0.3.
    - [Release notes](https://github.com/gradle/common-custom-user-data-maven-extension/releases)
    - [Commits](https://github.com/gradle/common-custom-user-data-maven-extension/compare/v2.0.2...v2.0.3)
    
    ---
    updated-dependencies:
    - dependency-name: com.gradle:common-custom-user-data-maven-extension
      dependency-version: 2.0.3
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump com.gradle:common-custom-user-data-maven-extension
    
    Bumps [com.gradle:common-custom-user-data-maven-extension](https://github.com/gradle/common-custom-user-data-maven-extension) from 2.0.2 to 2.0.3.
    - [Release notes](https://github.com/gradle/common-custom-user-data-maven-extension/releases)
    - [Commits](https://github.com/gradle/common-custom-user-data-maven-extension/compare/v2.0.2...v2.0.3)
    
    ---
    updated-dependencies:
    - dependency-name: com.gradle:common-custom-user-data-maven-extension
      dependency-version: 2.0.3
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump com.gradle:common-custom-user-data-maven-extension
    
    Bumps [com.gradle:common-custom-user-data-maven-extension](https://github.com/gradle/common-custom-user-data-maven-extension) from 2.0.2 to 2.0.3.
    - [Release notes](https://github.com/gradle/common-custom-user-data-maven-extension/releases)
    - [Commits](https://github.com/gradle/common-custom-user-data-maven-extension/compare/v2.0.2...v2.0.3)
    
    ---
    updated-dependencies:
    - dependency-name: com.gradle:common-custom-user-data-maven-extension
      dependency-version: 2.0.3
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump junit-jupiter.version in /build-caching-maven-samples
    
    Bumps `junit-jupiter.version` from 5.13.0 to 5.13.1.
    
    Updates `org.junit.jupiter:junit-jupiter-api` from 5.13.0 to 5.13.1
    - [Release notes](https://github.com/junit-team/junit5/releases)
    - [Commits](https://github.com/junit-team/junit5/compare/r5.13.0...r5.13.1)
    
    Updates `org.junit.jupiter:junit-jupiter-engine` from 5.13.0 to 5.13.1
    - [Release notes](https://github.com/junit-team/junit5/releases)
    - [Commits](https://github.com/junit-team/junit5/compare/r5.13.0...r5.13.1)
    
    ---
    updated-dependencies:
    - dependency-name: org.junit.jupiter:junit-jupiter-api
      dependency-version: 5.13.1
      dependency-type: direct:development
      update-type: version-update:semver-patch
    - dependency-name: org.junit.jupiter:junit-jupiter-engine
      dependency-version: 5.13.1
      dependency-type: direct:development
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- convention-develocity-shared/convention-develocity-maven-extension/build.gradle.kts
+++ convention-develocity-shared/convention-develocity-maven-extension/build.gradle.kts
@@ -12,7 +12,7 @@ dependencies {
     compileOnly("org.apache.maven:maven-core:3.9.10")
 
     implementation("com.gradle:develocity-maven-extension:2.0.1")
-    implementation("com.gradle:common-custom-user-data-maven-extension:2.0.2")
+    implementation("com.gradle:common-custom-user-data-maven-extension:2.0.3")
     implementation(project(":convention-develocity-common"))
 }
 
