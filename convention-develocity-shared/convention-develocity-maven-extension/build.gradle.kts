00c5948aa91b17306e57d774c5056e6065405be0
bot-gh-dv-shared <bot-gh-dv-shared+renovate@gradle.com>
2026-03-25 20:25:13 +0100

    [Renovate Bot] Update Gradle dependencies (#2228)
    Signed-off-by: Renovate Bot for Gradle <bot-gh-dv-shared+renovate@gradle.com>

--- convention-develocity-shared/convention-develocity-maven-extension/build.gradle.kts
+++ convention-develocity-shared/convention-develocity-maven-extension/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 dependencies {
     compileOnly("org.apache.maven:maven-core:3.9.14")
-    implementation("com.gradle:develocity-maven-extension:2.3.4")
+    implementation("com.gradle:develocity-maven-extension:2.4.0")
     implementation("com.gradle:common-custom-user-data-maven-extension:2.1.0")
     implementation(project(":convention-develocity-common"))
 }
